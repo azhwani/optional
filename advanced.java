67e1a8094cb10bf67b00502d1604eec4ef5c9ad8
Azhwani <azh.abderrahim@gmail.com>
2019-09-16 10:14:19 +0100

    Create advanced.java

---
+++ advanced.java
@@ -0,0 +1,33 @@
+package com.azhwani.optional;
+
+import java.util.Optional;
+
+public class AdvancedOptional {
+
+    public static void main(String[] args) {
+
+        Optional<String> op1 = Optional.of("Optional");
+        Optional<String> op2 = Optional.empty();
+        Integer value1 = null;
+        Optional<Integer> op3 = Optional.ofNullable(value1);
+
+        //get
+
+        //isPresent
+        
+        //ifPresent
+        
+        //filter && map
+        
+        //orElseGet
+        
+        
+        //orElse
+        
+        
+        //orElseThrow
+       
+
+    }
+
+}
